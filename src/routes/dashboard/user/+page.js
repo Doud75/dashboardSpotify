ba37bf39fb0f0cfdf1751ec50d12929567b23520
Doud75 <adrienquimbre@gmail.com>
2025-01-09 16:29:18 +0100

    feat: use chartJs in views

--- src/routes/dashboard/user/+page.js
+++ src/routes/dashboard/user/+page.js
@@ -2,10 +2,76 @@ import { getGenderStats, getAgeStats, getPremiumStats } from '@/lib/services/api
 
 /** @type {import('./$types').PageLoad} */
 export async function load() {
+	const ageStats = await getAgeStats();
+	const premiumStats = await getPremiumStats();
+	const genderStats = await getGenderStats();
+
+	const ageBarChartData = {
+		labels: Object.keys(ageStats.age_stats),
+		datasets: [
+			{
+				label: 'Distribution globale (%)',
+				data: Object.values(ageStats.age_stats).map(stat => stat.global_percentage),
+				backgroundColor: 'rgba(75, 192, 192, 0.5)',
+				borderColor: 'rgba(75, 192, 192, 1)',
+				borderWidth: 1
+			},
+			{
+				label: 'Premium (%)',
+				data: Object.values(ageStats.age_stats).map(stat => stat.premium_percentage),
+				backgroundColor: 'rgba(153, 102, 255, 0.5)',
+				borderColor: 'rgba(153, 102, 255, 1)',
+				borderWidth: 1
+			}
+		]
+	};
+
+	const planDonutChartData = {
+		labels: Object.keys(premiumStats.plan_distribution),
+		datasets: [
+			{
+				label: 'Distribution des abonnements',
+				data: Object.values(premiumStats.plan_distribution).map(plan => plan.count),
+				backgroundColor: [
+					'rgba(54, 162, 235, 0.5)',  // Bleu pour Free
+					'rgba(153, 102, 255, 0.5)'  // Violet pour Premium
+				],
+				borderColor: [
+					'rgba(54, 162, 235, 1)',
+					'rgba(153, 102, 255, 1)'
+				],
+				borderWidth: 1
+			}
+		]
+	};
+
+	const genderPieChartData = {
+		labels: Object.keys(genderStats.gender_stats),
+		datasets: [
+			{
+				label: 'Distribution par genre',
+				data: Object.values(genderStats.gender_stats).map(gender => gender.global_percentage),
+				backgroundColor: [
+					'rgba(54, 162, 235, 0.5)',  // Bleu pour Male
+					'rgba(255, 99, 132, 0.5)',  // Rose pour Female
+					'rgba(255, 206, 86, 0.5)'   // Jaune pour Others
+				],
+				borderColor: [
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 99, 132, 1)',
+					'rgba(255, 206, 86, 1)'
+				],
+				borderWidth: 1
+			}
+		]
+	};
 
 	return {
-		genderStats : await getGenderStats(),
-		ageStats : await getAgeStats(),
-		premiumStats : await getPremiumStats(),
+		genderStats,
+		ageStats,
+		ageBarChartData,
+		planDonutChartData,
+		genderPieChartData,
+		premiumStats
 	};
 }
--- src/routes/dashboard/user/+page.svelte
+++ src/routes/dashboard/user/+page.svelte
@@ -1,17 +1,62 @@
 <script>
 	import TextCard from '@/lib/components/ui/TextCard.svelte';
+	import BarChart from '@/lib/components/widgets/chart/BarChart.svelte';
+	import DoughnutChart from '@/lib/components/widgets/chart/DoughnutChart.svelte';
+	import PieChart from '@/lib/components/widgets/chart/PieChart.svelte';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
-	let genderStats = data.genderStats;
-	let ageStats = data.ageStats;
 	let premiumStats = data.premiumStats;
-	console.log(genderStats);
-	console.log(ageStats);
-	console.log(premiumStats);
+	let ageBarChartData = data.ageBarChartData;
+	let planDonutChartData = data.planDonutChartData;
+	let genderPieChartData = data.genderPieChartData;
 
 	const totalNb = premiumStats.plan_distribution.Free.count + premiumStats.plan_distribution.Premium.count;
 	const totalPremium = premiumStats.plan_distribution.Premium.count;
+
+	const barChartOptions = {
+		scales: {
+			y: {
+				beginAtZero: true,
+				max: 100,
+				title: {
+					display: true,
+					text: 'Pourcentage (%)',
+				},
+			},
+		},
+		plugins: {
+			legend: {
+				position: 'bottom',
+			},
+		},
+	};
+
+	const planDonutChartOptions = {
+		plugins: {
+			legend: {
+				display: true,
+				position: 'bottom',
+			},
+			title: {
+				display: true,
+				text: 'Distribution des abonnements',
+			},
+		},
+	};
+
+	const genderPieChartOptions = {
+		plugins: {
+			legend: {
+				display: true,
+				position: 'bottom',
+			},
+			title: {
+				display: true,
+				text: 'Distribution par genre',
+			},
+		},
+	};
 </script>
 
 <div class="dashboard">
@@ -29,10 +74,18 @@
 			value="{totalPremium} M"
 			description="Augmentation de 10%"
 		/>
-		<div class="span-1-3"></div>
-		<div class="span-1-3"></div>
-		<div class="span-2-4"></div>
-		<div class="span-2-2"></div>
+		<div class="span-1-3">
+			<DoughnutChart data={planDonutChartData} options={planDonutChartOptions} />
+		</div>
+		<div class="span-1-3">
+			<PieChart data={genderPieChartData} options={genderPieChartOptions} />
+		</div>
+		<div class="span-2-4">
+			<BarChart data={ageBarChartData} options={barChartOptions} />
+		</div>
+		<div class="span-2-2">
+			<BarChart data={ageBarChartData} options={barChartOptions} />
+		</div>
 	</div>
 </div>
 
@@ -54,27 +107,18 @@
 		gap: 1.5rem; // gap-6 equivalent
 		grid-auto-rows: minmax(100px, auto);
 
-		.span-1-1 {
-			grid-column: span 1;
-			grid-row: span 1;
-			background-color: #1db954;
-		}
-
 		.span-2-2 {
 			grid-column: span 2;
 			grid-row: span 2;
-			background-color: #1db954;
 		}
 
 		.span-2-4 {
 			grid-column: span 2;
 			grid-row: span 4;
-			background-color: #1db954;
 		}
 		.span-1-3 {
 			grid-column: span 1;
 			grid-row: span 3;
-			background-color: #1db954;
 		}
 	}
 </style>
