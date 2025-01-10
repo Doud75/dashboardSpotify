285ec545c9c8eff61e62547f5ec3b62c5e8ea393
Doud75 <adrienquimbre@gmail.com>
2025-01-10 11:41:50 +0100

    fix: componant used

--- src/routes/dashboard/track/+page.svelte
+++ src/routes/dashboard/track/+page.svelte
@@ -1,43 +1,49 @@
 <script>
-	import InfoBox from '$lib/components/trackComponents/InfoBox.svelte';
-	import Leaderboard from '$lib/components/trackComponents/Leaderboard.svelte';
+	import Leaderboard from '$lib/components/ui/Leaderboard.svelte';
 	import PolarAreaChart from '@/lib/components/widgets/chart/PolarAreaChart.svelte';
 	import BarChart from '@/lib/components/widgets/chart/BarChart.svelte';
+	import TextCard from '@/lib/components/ui/TextCard.svelte';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
 </script>
 
 <div class="grid-container">
-	<div class="title">Données sur les Tracks</div>
-
 	<div>
 		<div class="header">
-			<InfoBox
-				title="Chanson la plus populaire"
-				data={`${data.topStats.top_10_songs[0].track_name} by ${data.topStats.top_10_songs[0].artist_name}`}
-			/>
-			<InfoBox title="Langue la plus populaire" data={data.mostPopularLanguage} />
-			<InfoBox
-				title="Chanson la plus dansante"
-				data={`${data.topStats.top_10_danceable_songs[0].track_name} by ${data.topStats.top_10_danceable_songs[0].artist_name}`}
-			/>
+			<TextCard title="Chanson la plus populaire">
+				<div class="large-text">
+					{`${data.topStats.top_10_songs[0].track_name}`}
+				</div>
+			</TextCard>
+			<TextCard title="Langue la plus populaire">
+				<div class="large-text">
+					{data.mostPopularLanguage}
+				</div>
+			</TextCard>
+			<TextCard title="Chanson la plus dansante">
+				<div class="large-text">
+					{`${data.topStats.top_10_danceable_songs[0].track_name}`}
+				</div>
+			</TextCard>
 		</div>
 
 		<div class="main">
 			<div class="main-top">
 				<div class="block">
-					<h3>Popularité par langue</h3>
 					{#if data.chartDataPopularity}
-						<PolarAreaChart data={data.chartDataPopularity} />
+						<PolarAreaChart data={data.chartDataPopularity} title="Popularité par langue" />
 					{:else}
 						<p>Aucune donnée disponible pour le graphique.</p>
 					{/if}
 				</div>
 				<div class="block">
-					<h3>Dansabilité par langue</h3>
 					{#if data.danceabilityChartData}
-						<PolarAreaChart data={data.danceabilityChartData} options={data.polarAreaChartOptions} />
+						<PolarAreaChart
+							data={data.danceabilityChartData}
+							options={data.polarAreaChartOptions}
+							title="Dansabilité par langue"
+						/>
 					{:else}
 						<p>Aucune donnée disponible pour le graphique.</p>
 					{/if}
@@ -45,9 +51,8 @@
 			</div>
 
 			<div class="main-bottom">
-				<h3>Acousticness moyenne par tranche de popularité</h3>
 				{#if data.barChartData}
-					<BarChart data={data.barChartData} />
+					<BarChart data={data.barChartData} title="Acousticness moyenne par tranche de popularité" />
 				{:else}
 					<p>Aucune donnée disponible pour le graphique.</p>
 				{/if}
@@ -69,14 +74,6 @@
 		padding: 1rem;
 	}
 
-	.title {
-		grid-column: span 2;
-		text-align: center;
-		font-size: 1.8rem;
-		font-weight: bold;
-		margin-bottom: 1rem;
-	}
-
 	.header {
 		display: grid;
 		grid-template-columns: repeat(3, 1fr);
@@ -96,26 +93,8 @@
 		grid-gap: 1rem;
 	}
 
-	.main-bottom {
-		background: #fff;
-		padding: 1rem;
-		border-radius: 8px;
-		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-	}
-
-	.sidebar {
-		background: #f9f9f9;
-		padding: 1rem;
-		border-radius: 8px;
-		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-		height: auto;
-		align-self: start;
-	}
-
-	.block {
-		background: #fff;
-		padding: 1rem;
-		border-radius: 8px;
-		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	.large-text {
+		font-size: 1.2rem;
+		font-weight: bold;
 	}
 </style>
